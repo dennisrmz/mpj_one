59981e08b7710b8b947fba2e73ca8f9703a5a482
dennisrmz <orellanadennis12@gmail.com>
2021-10-09 07:41:02 -0600

    plugins

---
+++ wp-content/plugins/yith-woocommerce-product-add-ons/plugin-fw/dist/gutenberg/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('lodash', 'react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-polyfill', 'wp-url'), 'version' => '3e03d6f4dfaa9af2a7d13b1a840b3253');
