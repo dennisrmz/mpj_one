a34f71dd29af7e1119449e9f4e4b6631e29c6c57
Gonzalo <chalo543@hotmail.com>
2021-11-27 08:01:00 -0600

    "addons con variable global"

--- wp-content/plugins/newsletter-optica/assets/mpj_products_add_ons.js
+++ wp-content/plugins/newsletter-optica/assets/mpj_products_add_ons.js
@@ -1,6 +1,6 @@
 (function ($) {
 
-  function sendFeeWoo() { 
+  function sendFeeWoo() {
     // let total_cur_price = VCBoxes.CalcCurrentFob();  //Calcular monto total de precio total_fob_user 
 
     var form = {
@@ -22,83 +22,200 @@
       // showAlertMsg("Error al guardar los totales, recargue el sitio", operacion.DEFAULT, operacionStatus.FAIL);
     });
   }
+  var tipoLente = "";
+  var tipoFiltro = "";
+  var precioExtra = "";
+  var a = 0;
+  var datos = {
+    'producto': "",
+    'precioExtra':0,
+    'tipoLente': "",
+    'tipoFiltro': "",
+    'receta': {
+      "od_EST":"",
+      "od_CL": "",
+      "od_EJE": "",
+      "od_ADICION":"",
+      "od_TIPO": "",
+      "os_EST":"",
+      "os_CL": "",
+      "os_EJE": "",
+      "os_ADICION": "",
+      "os_TIPO":"",
+    }
+  }
+  var pivote = 0;
+  $(document).ready(function () {
 
-    var a = 0;
-    $(document).ready(function () {
-        sendFeeWoo()
-        console.log('producto_actual');
-        console.log(mpj_obj.products_in_cart);
+    var vcLentes = {
+      lentes: [],
+      CurrentLentes: null,
+      OnlyOneLentes: false, //Notifica que si hay una unica caja en Ls o en el server 
+      Init: function () {
+				let lentes_stg = localStorage.getItem("mpj_lentes"); //siempre sera un arreglo pero debe tener solo un item 	 
+				if (lentes_stg == null || lentes_stg.length == 0) {
+					this.OnlyOneLentes = true; //OnlyOneLentes se deja como true dado que no hay nada en ls
+					return false;
+				};
 
-       // alert('estoy cargando en producto');      
-       $(".radio_receta").click(function(){
-        var valor = $(this).val();
-        console.log(valor)
-          if(valor == 1)
-          {
-            $("#tablaReceta").css("display", "block");  
-          }else{
-            $("#tablaReceta").css("display", "none");  
-          }
-      
-       });
+				let LentesData = JSON.parse(lentes_stg);
 
-       $('#anterior').click(function () {
-        console.log(a)
-        if(a==1){
-         $("#tipoLent").css("display", "block");
-         $("#tipoFilt").css("display", "none");
-          a--;
-         }else{
-         if(a==2){
-            $("#tipoFilt").css("display", "block");
-           $("#receta").css("display", "none");
-           a--;
-           var receta = {
-            "od_EST":$("#od_EST").val(),
-            "od_CL":$("#od_CL").val(),
-            "od_EJE":$("#od_EJE").val(),
-            "od_ADICION":$("#od_ADICION").val(),
-            "od_TIPO":$("#od_TIPO").val(),
-            "os_EST":$("#os_EST").val(),
-            "os_CL":$("#os_CL").val(),
-            "os_EJE":$("#os_EJE").val(),
-            "os_ADICION":$("#os_ADICION").val(),
-            "os_TIPO":$("#os_TIPO").val(),
-            }
-          
-            console.log(receta);
-            }
-        }
-        });
-       $('#siguiente').click(function () {
+				if (LentesData.length == 1) { //Si y solo si hay una caja 
+					this.OnlyOneLentes = true;
+					this.lentes = LentesData;
+					this.CurrentLentes = LentesData[0];
+				};
+
+				if (LentesData.length == 0) {
+					this.OnlyOneLentes = true;
+					this.CurrentLentes = null;
+				}
+			},
+      Insert: function (data) {
+				let NewLente = data				
+					this.lentes.push(NewLente); //Se agrego la caja 
+          console.log(this.lentes);
          
-        if(a==0){
+					return true;				
+			},
+      Save: function () {
+				//this.Boxes[0] = this.CurrentBox;
+				//localStorage.setItem("vclobi-boxes", JSON.stringify(this.Boxes));
+				localStorage.setItem("mpj_lentes", JSON.stringify(this.lentes));
+			},
+      
+    } 
+
+
+    /******* */
+    vcLentes.Init();
+    console.log(vcLentes);
+    sendFeeWoo()
+    console.log('producto_actual');
+    console.log(mpj_obj.mpj_current_prod);
+    console.log('Productos en carrito');
+    console.log(mpj_obj.products_in_cart);
+    console.log('Rangos');
+    console.log(mpj_obj.limites_rango);
+    // alert('estoy cargando en producto');      
+    $(".radio_receta").click(function () {
+      var valor = $(this).val();
+      console.log(valor)
+      if (valor == 1) {
+        $("#tablaReceta").css("display", "block");
+      } else {
+        $("#tablaReceta").css("display", "none");
+      }
+
+    });
+
+    $('#anterior').click(function () {
+      console.log(a)
+      if (a == 1) {
+        $("#tipoLent").css("display", "block");
+        $("#tipoFilt").css("display", "none");
+        a--;
+      } else {
+        if (a == 2) {
+          $("#tipoFilt").css("display", "block");
+          $("#receta").css("display", "none");
+          $("#siguiente").css("display", "block");
+          $("#guardar").css("display", "none");
+          a--;
+          var receta = {
+            "od_EST": $("#od_EST").val(),
+            "od_CL": $("#od_CL").val(),
+            "od_EJE": $("#od_EJE").val(),
+            "od_ADICION": $("#od_ADICION").val(),
+            "od_TIPO": $("#od_TIPO").val(),
+            "os_EST": $("#os_EST").val(),
+            "os_CL": $("#os_CL").val(),
+            "os_EJE": $("#os_EJE").val(),
+            "os_ADICION": $("#os_ADICION").val(),
+            "os_TIPO": $("#os_TIPO").val(),
+          }
+         
+          console.log(receta);
+        }
+      }
+    });
+    $('#siguiente').click(function () {
+
+      if (a == 0) {
         $("#tipoLent").css("display", "none");
         $("#tipoFilt").css("display", "block");
         a++;
-        
-         }
-       else{
-         if(a==1){
-            $("#tipoFilt").css("display", "none");
-            $("#receta").css("display", "block");
-            a++;
-             }
-            }          
-          });
-    
+
+      }
+      else {
+        if (a == 1) {
+          $("#tipoFilt").css("display", "none");
+          $("#receta").css("display", "block");
+          $("#siguiente").css("display", "none");
+          $("#guardar").css("display", "block");
+          a++;
+        }
+      }
     });
 
-    $(".tipoLentes").click(function(){
-      var tipoLente = $(this).val();
-      console.log("Tipo de lente "+ tipoLente);
-    
-     });
-     $(".tipoFiltro").click(function(){
-      var tipoFiltro = $(this).val();
-      console.log("Tipo de filtro "+ tipoFiltro);     
-    
-     });
+
+    $('#guardar').click(function () {
      
-  
+      $("#siguiente").css("display", "none");
+      $("#guardar").css("display", "none");
+      for(var i=0;i<mpj_obj.limites_rango.length;i++)
+      {
+       
+        if((parseFloat($("#od_ADICION").val()) > parseFloat(mpj_obj.limites_rango[i].min_limit) && parseFloat($("#od_ADICION").val()) < parseFloat(mpj_obj.limites_rango[i].max_limit))
+         ||(parseFloat(($("#os_ADICION").val()) > parseFloat(mpj_obj.limites_rango[i].min_limit) && parseFloat($("#os_ADICION").val() < mpj_obj.limites_rango[i].max_limit))))
+         {
+          precioExtra = mpj_obj.limites_rango[i].precio_extra
+          console.log(mpj_obj.limites_rango[i].precio_extra);
+         }
+        console.log( mpj_obj.limites_rango[i].min_limit);
+      }
+          datos.producto=mpj_obj.mpj_current_prod,
+          datos.tipoLente=tipoLente,
+          datos.tipoFiltro=tipoFiltro,
+          datos.precioExtra = precioExtra,
+          datos.receta.od_EST= $("#od_EST").val()
+          datos.receta.od_CL= $("#od_CL").val()
+          datos.receta.od_EJE= $("#od_EJE").val()
+          datos.receta.od_ADICION= $("#od_ADICION").val()
+          datos.receta.od_TIPO= $("#od_TIPO").val()
+          datos.receta.os_EST= $("#os_EST").val()
+          datos.receta.os_CL= $("#os_CL").val()
+          datos.receta.os_EJE= $("#os_EJE").val()
+          datos.receta.os_ADICION= $("#os_ADICION").val()
+          datos.receta.os_TIPO= $("#os_TIPO").val()      
+        if(pivote == 0)
+        {
+          vcLentes.Insert(datos);          
+          vcLentes.Save();
+          
+          pivote++;
+        }else{
+         
+         // delete 
+          vcLentes.Insert(datos);
+          push
+        }
+      console.log(precioExtra);
+      console.log(datos);
+    });
+
+  });
+
+  $(".tipoLentes").click(function () {
+    tipoLente = $(this).val();
+    console.log("Tipo de lente " + tipoLente);
+
+  });
+  $(".tipoFiltro").click(function () {
+    tipoFiltro = $(this).val();
+    console.log("Tipo de filtro " + tipoFiltro);
+
+  });
+
+
 })(jQuery);
--- wp-content/plugins/newsletter-optica/template/mpj-add-ons.php
+++ wp-content/plugins/newsletter-optica/template/mpj-add-ons.php
@@ -2,178 +2,221 @@
     <div id="tipoLent" style="display:block;">
         <h4>Elegi tu tipo de lente</h4>
         <br>
-        <?php       
-            $args = array(
-                'post_type' => 'opticampj_add_ons',
-                'nopaging'  => true
-            );
+        <?php
+        $args = array(
+            'post_type' => 'opticampj_add_ons',
+            'nopaging'  => true
+        );
 
-            $posts_type_tablas = new WP_Query($args);
-            $a=1;
-            while( $posts_type_tablas->have_posts() ): $posts_type_tablas->the_post();
+        $posts_type_tablas = new WP_Query($args);
+        $a = 1;
+        while ($posts_type_tablas->have_posts()) : $posts_type_tablas->the_post();
 
-                if(get_field('tipo_add_on') == "tipo_lente"):
-                    
-                   
-                    ?>
-                   
-                        <div style="
+            if (get_field('tipo_add_on') == "tipo_lente") :
+
+
+        ?>
+
+                <div style="
                             display: flex;
                             justify-content: flex-start;
                             align-items: center;
-                            flex-direction: row;"
-                        >
-                            <div class="image position-above">
-                                <img style="width:50%;" src="<?php echo wp_get_attachment_image_src( get_field('imagen') , 'thumbnail' )[0]; ?>">
-                            </div>
-                            <div>
-                                <div class="form-check">
-                                    <input class="form-check-input tipoLentes" type="radio" data-value="<?php echo get_field('aumento_precio_por_rango'); ?>" name="<?php echo get_field('tipo_add_on'); ?>" id="<?php echo get_field('tipo_add_on') . "_" .  get_the_ID(); ?>" value="<?php the_ID(); ?>">
-                                    <label class="form-check-label tipoLentes" for="<?php echo get_field('nombre'); ?>">
-                                        <?php echo get_field('nombre'); ?>
-                                    </label>
-                                </div>
-                                <div>
-                                    Precio Extra: <?php echo get_field('precio_extra'); ?>
-                                </div>
-                            </div>
-
-                            
+                            flex-direction: row;">
+                    <div class="image position-above">
+                        <img style="width:50%;" src="<?php echo wp_get_attachment_image_src(get_field('imagen'), 'thumbnail')[0]; ?>">
+                    </div>
+                    <div>
+                        <div class="form-check">
+                            <input class="form-check-input tipoLentes" type="radio" data-value="<?php echo get_field('aumento_precio_por_rango'); ?>" name="<?php echo get_field('tipo_add_on'); ?>" id="<?php echo get_field('tipo_add_on') . "_" .  get_the_ID(); ?>" value="<?php the_ID(); ?>">
+                            <label class="form-check-label tipoLentes" for="<?php echo get_field('nombre'); ?>">
+                                <?php echo get_field('nombre'); ?>
+                            </label>
                         </div>
-                        <br>
+                        <div>
+                            Precio Extra: <?php echo get_field('precio_extra'); ?>
+                        </div>
+                    </div>
 
-                    <?php
 
-                endif;
+                </div>
+                <br>
 
-            endwhile;
+        <?php
 
-            /******************* Obteniendo PostTypes Meriendas  ***********************/
-            wp_reset_postdata();
-                2509
+            endif;
+
+        endwhile;
+
+        /******************* Obteniendo PostTypes Meriendas  ***********************/
+        wp_reset_postdata();
+        2509
         ?>
-    </div>         
-    <div  id="tipoFilt" style="display:none;">
+    </div>
+    <div id="tipoFilt" style="display:none;">
         <h4>Elegi tu tipo de Filtro</h4>
         <br>
-        <?php       
-            $args = array(
-                'post_type' => 'opticampj_add_ons',
-                'nopaging'  => true
-            );
+        <?php
+        $args = array(
+            'post_type' => 'opticampj_add_ons',
+            'nopaging'  => true
+        );
 
-            $posts_type_tablas = new WP_Query($args);
+        $posts_type_tablas = new WP_Query($args);
 
-            while( $posts_type_tablas->have_posts() ): $posts_type_tablas->the_post();
+        while ($posts_type_tablas->have_posts()) : $posts_type_tablas->the_post();
 
-                if(get_field('tipo_add_on') == "tipo_filtro"):
+            if (get_field('tipo_add_on') == "tipo_filtro") :
 
-                    ?>
-                        <div style="
+        ?>
+                <div style="
                             display: flex;
                             justify-content: flex-start;
                             align-items: center;
-                            flex-direction: row;"
-                        >
-                            <div class="image position-above">
-                                <img style="width:50%;" src="<?php echo wp_get_attachment_image_src( get_field('imagen') , 'thumbnail' )[0]; ?>">
-                            </div>
-                            <div>
-                                <div class="form-check">
-                                    <input class="form-check-input tipoFiltro" type="radio" name="<?php echo get_field('tipo_add_on'); ?>" id="<?php echo get_field('tipo_add_on') . "_" .  get_the_ID(); ?>" value="<?php the_ID(); ?>">
-                                    <label class="form-check-label" for="<?php echo get_field('nombre'); ?>">
-                                        <?php echo get_field('nombre'); ?>
-                                    </label>
-                                </div>
-                                <div>
-                                    Precio Extra: <?php echo get_field('precio_extra'); ?>
-                                </div>
-                            </div>
-
-                            
+                            flex-direction: row;">
+                    <div class="image position-above">
+                        <img style="width:50%;" src="<?php echo wp_get_attachment_image_src(get_field('imagen'), 'thumbnail')[0]; ?>">
+                    </div>
+                    <div>
+                        <div class="form-check">
+                            <input class="form-check-input tipoFiltro" type="radio" name="<?php echo get_field('tipo_add_on'); ?>" id="<?php echo get_field('tipo_add_on') . "_" .  get_the_ID(); ?>" value="<?php the_ID(); ?>">
+                            <label class="form-check-label" for="<?php echo get_field('nombre'); ?>">
+                                <?php echo get_field('nombre'); ?>
+                            </label>
                         </div>
-                        <br>
+                        <div>
+                            Precio Extra: <?php echo get_field('precio_extra'); ?>
+                        </div>
+                    </div>
 
-                    <?php
 
-                endif;
+                </div>
+                <br>
 
-            endwhile;
+        <?php
 
-            /******************* Obteniendo PostTypes Meriendas  ***********************/
-            wp_reset_postdata();
-                
+            endif;
+
+        endwhile;
+
+        /******************* Obteniendo PostTypes Meriendas  ***********************/
+        wp_reset_postdata();
+
         ?>
-    </div> 
-    <div  id="receta" style="display:none;">
+    </div>
+    <div id="receta" style="display:none;">
         <h4>Dejanos Saber un poco mas</h4>
         <p>¿Posees Receta de Lentes?</p>
         <div class="form-check">
-            <input class="form-check-input radio_receta" type="radio" name="mpjreceta" id="receta_si" value="1" >
-            <label class="form-check-label" for="si_receta">Si poseeo</label> 
+            <input class="form-check-input radio_receta" type="radio" name="mpjreceta" id="receta_si" value="1">
+            <label class="form-check-label" for="si_receta">Si poseeo</label>
         </div>
 
         <h4>Como leer tu receta</h4>
-        <a href="<?php echo esc_url(  get_permalink( get_page_by_title( 'Como leer tu receta oftalmológica' ) ) ); ?>" target="_blank"><?php esc_html_e( 'Como leer tu receta oftalmológica', 'textdomain' ); ?></a>
+        <a href="<?php echo esc_url(get_permalink(get_page_by_title('Como leer tu receta oftalmológica'))); ?>" target="_blank"><?php esc_html_e('Como leer tu receta oftalmológica', 'textdomain'); ?></a>
 
         <div style="display: flex; justify-content: flex-start; align-items: center; flex-direction: row; display:none; " id="tablaReceta">
 
             <style type="text/css">
-                .tg  {border-collapse:collapse;border-spacing:0;}
-                .tg td{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
-                overflow:hidden;padding:10px 5px;word-break:normal;}
-                .tg th{border-color:black;border-style:solid;border-width:1px;font-family:Arial, sans-serif;font-size:14px;
-                font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
-                .tg .tg-zv4m{border-color:#000;text-align:left;vertical-align:top}
-                .tg .tg-0lax{text-align:left;vertical-align:top;padding:0;max-width: 0.7rem;}
+                .tg {
+                    border-collapse: collapse;
+                    border-spacing: 0;
+                }
+
+                .tg td {
+                    border-color: black;
+                    border-style: solid;
+                    border-width: 1px;
+                    font-family: Arial, sans-serif;
+                    font-size: 14px;
+                    overflow: hidden;
+                    padding: 10px 5px;
+                    word-break: normal;
+                }
+
+                .tg th {
+                    border-color: black;
+                    border-style: solid;
+                    border-width: 1px;
+                    font-family: Arial, sans-serif;
+                    font-size: 14px;
+                    font-weight: normal;
+                    overflow: hidden;
+                    padding: 10px 5px;
+                    word-break: normal;
+                }
+
+                .tg .tg-zv4m {
+                    border-color: #000;
+                    text-align: left;
+                    vertical-align: top
+                }
+
+                .tg .tg-0lax {
+                    text-align: left;
+                    vertical-align: top;
+                    padding: 0;
+                    max-width: 0.7rem;
+                }
             </style>
-                <table class="tg" >
+            <table class="tg">
                 <thead>
-                <tr>
-                    <th class="tg-zv4m"></th>
-                    <th class="tg-zv4m">EST</th>
-                    <th class="tg-zv4m">CL</th>
-                    <th class="tg-zv4m">EJE</th>
-                    <th class="tg-zv4m">ADICION</th>
-                    <th class="tg-zv4m">TIPO</th>
-                </tr>
+                    <tr>
+                        <th class="tg-zv4m"></th>
+                        <th class="tg-zv4m">EST</th>
+                        <th class="tg-zv4m">CL</th>
+                        <th class="tg-zv4m">EJE</th>
+                        <th class="tg-zv4m">ADICION</th>
+                        <th class="tg-zv4m">TIPO</th>
+                    </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td class="tg-0lax">O.D</td>
-                    <td class="tg-0lax od"><input type="number" name="OD_EST" id="od_EST" style="width: 100%;">  </td>
-                    <td class="tg-0lax"><input type="number" name="OD_EST" id="od_CL"  style="width: 100%;">  </td>
-                    <td class="tg-0lax"><input type="number" name="OD_EST" id="od_EJE"  style="width: 100%;">  </td>
-                    <td class="tg-0lax"><input type="number" name="OD_EST" id="od_ADICION"  style="width: 100%;">  </td>
-                    <td class="tg-0lax"><input type="number" name="OD_EST" id="od_TIPO"  style="width: 100%;">  </td>
-                </tr>
-                <tr>
-                    <td class="tg-0lax">O.S</td>
-                    <td class="tg-0lax"><input type="number" name="OD_EST" id="os_TIPO" style="width: 100%;">  </td>
-                    <td class="tg-0lax"><input type="number" name="OD_EST" id="os_CL" style="width: 100%;">  </td>
-                    <td class="tg-0lax"><input type="number" name="OD_EST" id="os_EJE"  style="width: 100%;">  </td>
-                    <td class="tg-0lax"><input type="number" name="OD_EST" id="os_ADICION" style="width: 100%;">  </td>
-                    <td class="tg-0lax"><input type="number" name="OD_EST" id="os_TIPO" style="width: 100%;">  </td>
-                </tr>
+                    <tr>
+                        <td class="tg-0lax">O.D</td>
+                        <td class="tg-0lax od"><input type="number" name="OD_EST" id="od_EST" style="width: 100%;"> </td>
+                        <td class="tg-0lax"><input type="number" name="OD_EST" id="od_CL" style="width: 100%;"> </td>
+                        <td class="tg-0lax"><input type="number" name="OD_EST" id="od_EJE" style="width: 100%;"> </td>
+                        <td class="tg-0lax"><input type="number" name="OD_EST" id="od_ADICION" style="width: 100%;"> </td>
+                        <td class="tg-0lax"><input type="text" name="OD_EST" id="od_TIPO" style="width: 100%;"> </td>
+                    </tr>
+                    <tr>
+                        <td class="tg-0lax">O.S</td>
+                        <td class="tg-0lax"><input type="number" name="OD_EST" id="os_TIPO" style="width: 100%;"> </td>
+                        <td class="tg-0lax"><input type="number" name="OD_EST" id="os_CL" style="width: 100%;"> </td>
+                        <td class="tg-0lax"><input type="number" name="OD_EST" id="os_EJE" style="width: 100%;"> </td>
+                        <td class="tg-0lax"><input type="number" name="OD_EST" id="os_ADICION" style="width: 100%;"> </td>
+                        <td class="tg-0lax"><input type="text" name="OD_EST" id="os_TIPO" style="width: 100%;"> </td>
+                    </tr>
                 </tbody>
-                </table>
+            </table>
 
         </div>
 
 
         <div class="form-check">
             <input class="form-check-input radio_receta" type="radio" name="mpjreceta" id="receta_si1" value="0">
-            <label class="form-check-label" for="no_receta">No poseo</label> 
+            <label class="form-check-label" for="no_receta">No poseo</label>
         </div>
-            <a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Reserva Cita' ) ) ); ?>" target="_blank"><?php esc_html_e( 'Reserva Cita', 'textdomain' ); ?></a>
-            *Terminos: En la evalucion pueden haber cargo adicionales
-    </div> 
-    <div>
-    <button type="button" id="anterior" class="btn btn-sm  btn-success"> Anterior </button>
-    <button type="button" id="siguiente" class="btn btn-sm  btn-success"> Siguiente </button>
+        <a href="<?php echo esc_url(get_permalink(get_page_by_title('Reserva Cita'))); ?>" target="_blank"><?php esc_html_e('Reserva Cita', 'textdomain'); ?></a>
+        *Terminos: En la evalucion pueden haber cargo adicionales
     </div>
     
+    <div>
+        <table style="border: hidden">          
+            <tr style="border: hidden">
+            <td style="border: hidden">
+            <button style="display:block;" type="button" id="anterior" class="btn btn-sm  btn-success"> Anterior </button>
+            </td>
+            <td style="border: hidden">
+            <button style="display:block;" type="button" id="siguiente" class="btn btn-sm  btn-success"> Siguiente </button>
+        <button style="display:none;" type="button" id="guardar" class="btn btn-sm  btn-success"> Guardar </button>
+            </td>
+            </tr>
+        </table>
+        
+       
+    </div>
 
 
-   
+
+
 </div>
